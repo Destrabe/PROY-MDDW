49a967d6dde9a6db3c9e5445a79a4669eea2d693
Dajhana03 <dayana12.huaccha@gmail.com>
2026-09-03 01:39:57 -0500

    changing color on contact

--- components/header.jsx
+++ components/header.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="bg-white shadow-md sticky top-0 z-50 w-full">
-      {/* Fondo cambiado a blanco e íconos/textos a oscuros */}
+
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="shrink-0 flex items-center">
@@ -49,7 +49,7 @@ export default function Header() {
               </svg>
             </button>
 
-            <button className="hover:text-white transition-colors focus:outline-none">
+            <button className="hover:text-black transition-colors focus:outline-none">
               <svg className="w-6 h-6" fill="currentColor">
                 <use href="/sprite.svg#user" />
               </svg>
